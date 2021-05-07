c329a832c752c228f7d0938c382d4eb421bf89a0
Pouya Mohammadi <pouya.mohammadi@postgrad.manchester.ac.uk>
2021-05-07 15:29:02 +0100

    Added TicketDirector and moved the logic from controller to director

---
+++ src/main/java/Team5/onlinebookingsystem/TicketDirector.java
@@ -0,0 +1,35 @@
+package Team5.onlinebookingsystem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TicketDirector {
+
+
+    public Ticket makeTicket(TicketBuilder ticketBuilder, TicketService tService, String seatClass, String meal, String luggage, String priceBought, String insurance, String ageGroup, long flightID){
+
+        /// building new random non existent booking refs - ck
+        String bookingRef = tService.buildRandomTicketRef();
+        while (tService.findByTicketRef(bookingRef).equals("yes")) {
+            bookingRef = tService.buildRandomTicketRef();
+        }
+        //adding mandatory parameters
+        ticketBuilder.addBookingRef(bookingRef);
+        ticketBuilder.addSeatClass(seatClass);
+        ticketBuilder.addMeal(meal);
+        ticketBuilder.addPriceBought(priceBought);
+        ticketBuilder.addAgeGroup(ageGroup);
+        ticketBuilder.addFlightId(flightID);
+
+        //adding optional parameters
+        if (insurance.equals("yes")) {
+            ticketBuilder.addInsurance(insurance);
+        }
+        if (!"".equals(luggage) && !"0".equals(luggage)) {
+            ticketBuilder.addLuggage(luggage);
+        }
+
+        return ticketBuilder.getTicket();
+
+
+    }
+}
